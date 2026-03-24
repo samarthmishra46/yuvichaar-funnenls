b236845d7fe0e5937f5b03e26da3e90081c1051c
Samarth mishra <samarthmishra46@gmail.com>
2026-03-24 20:54:31 +0530

    new

--- src/components/sections/MeetTheFounder.tsx
+++ src/components/sections/MeetTheFounder.tsx
@@ -10,22 +10,21 @@ const coreCrew = [
   {
     title: 'Creative Heads',
     members: [
-      { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1773718060/72086315114_zone93.png' },
+      { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774364307/619831290_18103569937834074_5186660576995089904_n_in0bxo.jpg' },
       { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/c_crop,g_north_west,h_234,w_257,x_269,y_276/IMG_20250415_161554_Original_-_Kuldeep_Singh_Rathore_iboclu.jpg' },
       { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774110452/Screenshot_from_2026-03-21_21-57-04_zkx3zo.png' },
       
     ],
   },
-  // {
-  //   title: 'Core Operations',
-  //   members: [
-  //      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+  {
+    title: 'Core Operations',
+    members: [
+        
+        { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1773718060/72086315114_zone93.png' },
+          { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774364163/464366908_1652314448661438_707654230756181391_n_kanczk.jpg' },
       
-  //   ],
-  // },
+    ],
+  },
   {
     title: 'Tech & AI',
     members: [
@@ -39,35 +38,52 @@ const coreCrew = [
 
 const otherCrew = [
   {
-    title: 'The Models / Influencers',
+    title: 'Female Creators',
     members: [
-       { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+        { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774364542/519670112_18402531883116862_5294996964425303672_n_hz210t.jpg' },
+          { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774364813/638873799_18155773501440593_235831887062821038_n_sxghxr.jpg' },
+            { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774364886/652765149_18364089253200764_1629557260242311446_n_cthwgz.jpg' },
+              { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365120/548518163_17992561097832802_8159819587310002180_n_tjummw.jpg' },
+                { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365120/629849312_18450393856100087_1316244282805537861_n_tyu3je.jpg' },
+                  { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365120/519682291_17866106991422537_7427448598937310435_n_v2mtpw.jpg' },
+                    { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365121/650969140_18093099449471293_3192646460394751713_n_rgkcfv.jpg' },
+                      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+                        { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365120/499202352_18196232164308736_2469038785588982259_n_ocxtbr.jpg' },
+                          { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365126/473811164_1017321807107262_8153379905253506214_n_jyghxj.jpg' },
+                            { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365126/539824208_18287505547260305_5572622074118044576_n_i9oycq.jpg' },
+      
+    ],
+  },
+    {
+    title: 'Male Creators',
+    members: [
+       { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365337/645863158_18410230489135634_7526657969673381952_n_c9hxrf.jpg' },
+        { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365338/530426051_18421253629097356_3971499814476990179_n_jut2tt.jpg' },
+          { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365338/631433236_17899650888385501_4237069391668730474_n_wmrqbu.jpg' },
+            { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365339/648269821_18108577000763858_1056069114369062278_n_mgtzcz.jpg' },
+            
       
     ],
   },
   {
     title: 'Camera Crew',
     members: [
-     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+     { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365493/514466904_18305278771245246_2288304220599287626_n_e9k6vl.jpg' },
+      { image: 'https://res.cloudinary.com/dvxqb1wge/image/upload/v1774365573/589372716_18025873859788316_1385250804999834378_n_rneqtq.jpg' },
+      
       
     ],
   },
-  {
-    title: 'Video Editing',
-    members: [
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
-      { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+  // {
+  //   title: 'Video Editing',
+  //   members: [
+  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
+  //     { image: 'https://res.cloudinary.com/dix4pzu0k/image/upload/v1773218906/image_3006_qhiilf.png' },
       
-    ],
-  },
+  //   ],
+  // },
 ];
 
 
@@ -97,7 +113,7 @@ export default function MeetTheFounder() {
               {/* LinkedIn Badge */}
               
               <a
-                href="#"
+                href="https://www.linkedin.com/in/yuvraj-singh-rajawat-2992081b3/"
                 className="absolute top-4 right-4 w-10 h-10 rounded-full bg-[#0077B5] flex items-center justify-center text-white shadow-lg hover:scale-110 transition-transform"
               >
                 <Linkedin className="w-5 h-5" />
@@ -180,7 +196,7 @@ export default function MeetTheFounder() {
             </div>
 
             {/* The Other Crew */}
-            {/* <div className="pt-6">
+            <div className="pt-6">
               <div className="flex items-center gap-4 mb-6">
                 <div className="flex-1 h-px bg-border"></div>
                 <span className="text-xs font-semibold tracking-wider text-primary uppercase">The Other Crew</span>
@@ -201,7 +217,7 @@ export default function MeetTheFounder() {
                   </div>
                 ))}
               </div>
-            </div> */}
+            </div>
 
             {/* Highlights */}
             <CTAWithIdealFor className="mt-8" />
