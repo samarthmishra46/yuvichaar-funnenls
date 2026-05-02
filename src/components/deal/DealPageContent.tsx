92fd09eafeef478b383cff455cec9513606d1d7c
Samarth mishra <samarthmishra46@gmail.com>
2026-05-02 21:41:53 +0530

    new

--- src/components/deal/DealPageContent.tsx
+++ src/components/deal/DealPageContent.tsx
@@ -559,7 +559,7 @@ export default function DealPageContent({ data, token }: Props) {
                   <div className="tc-when">Day 30</div>
                   <div className="tc-title">Balance payment</div>
                   <div className="tc-amount">₹{formatCurrency(data.balanceWithGst)}</div>
-                  <div className="tc-note">Remaining {Math.round((data.balanceAmount / data.fixedFee) * 100)}% of fixed fee (₹{formatCurrency(data.balanceAmount)}) + 18% GST<br/>Due after 30 days of delivered work</div>
+                  <div className="tc-note">Remaining {Math.round((data.balanceAmount / data.fixedFee) * 100)}% of fixed fee (₹{formatCurrency(data.balanceAmount)}) + 18% GST<br/></div>
                 </div>
               </div>
               {data.hasPerformanceFee && (
@@ -922,7 +922,7 @@ export default function DealPageContent({ data, token }: Props) {
           <svg width="18" height="18" viewBox="0 0 24 24" fill="white"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51a5.5 5.5 0 00-.57-.01c-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479s1.065 2.875 1.213 3.074c.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z"/><path d="M11.999 2.003a9.996 9.996 0 00-8.591 15.077L2 22l5.093-1.336A9.996 9.996 0 1011.999 2.003z"/></svg>
           WhatsApp Yuvraj directly
         </a>
-        <a className="call-btn" href="tel:+919999900001">
+        <a className="call-btn" href="tel:+917023937272">
           <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>
           Call directly
         </a>
